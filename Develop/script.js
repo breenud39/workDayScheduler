cf00943e940ff9fb070125314bdc9c6e77eca806
Vaggelis <evangelodamianos@gmail.com>
2021-11-21 21:39:28 -0500

    functionality working

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,52 @@
-var day = "use moment js to get teh day attribute";
-var month = "Use moment to get the month attribute";
-var dayNum = "Use moment to get number day";
-
-var hoursOfDay = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-
 $(document).ready(function()
 {
-  // var m = moment().utcOffset(0);
-  // m.set({hour:14,minute:0,second:0})
-  // m.toISOString()
-  // m.format()
-  // console.log(m);
   var format = 'hh:mm:ss'
-  var currentTime = moment('10:53:00',format),
-  startTime = moment('09:00:00', format),
-  endTime = moment('17:00:00', format);
-  if (currentTime.isBetween(startTime, endTime)) {
-
-    console.log('is between')
-  
-  } else {
-  
-    console.log('is not between')
-  
-  }
+  var presentTime = moment().format("HH:mm:ss");
+  console.log(presentTime);
+  var startTime = moment('09:00:00', format);
+  var endTime = moment('17:00:00', format);
+  let currentTime =moment().format('HH');
+  console.log(presentTime);
   for (let i = 0; i <= 8; i++)
   {
-    // let time = moment().hour(9).add(i, 'hours').format("hA")
-    let currentTime = moment().format("hA")
-    // console.log(currentTime>time);
-    // console.log(time)
-    let parentElement = $("div").addClass("row parent");
-    let para = $("<p></p>").text(moment().hour(9).add(i, 'hours').format("hA"));
+    // if (presentTime.isBetween(startTime, endTime)) {
 
-    para.addClass("col-md-3");
-    let textArea = $("<textarea 'style= padding left: 100px' />").addClass("col-md-6");
-    let linkElement = $("<button><img src='./Develop/img/lock.jpg' height=50px width=50px /></button>").addClass("col-md-3");
-    linkElement.on("click", function()
-    {
-      console.log("button clicked");
-      console.log(textArea.val());
-      localStorage.setItem("textAreaValue"+i,textArea.val());
-    });
-    textArea.html(localStorage.getItem("textAreaValue"+i));
-    $(".container").append(parentElement, para, textArea, linkElement);
+      console.log('is between')
+        if(i < currentTime -9)
+        {
+          // color grey
+          createElementsAsPerTime("past",i)
+        }else if(i === currentTime - 9)
+        {
+          // color red
+          createElementsAsPerTime("present",i)
+        }
+        else if(i > currentTime -9){
+          // color green
+          createElementsAsPerTime("future",i)
+        }
+    // } else {
+      // createElementsAsPerTime("outsideTimeZone",i)
+    // }
   }
   let currentDay = $("#currentDay").text(moment().format("dddd, MMM Do"));
   $("#jumbotron").append(currentDay);
 });
+
+function createElementsAsPerTime(time,i){
+  let parentElement = $("div").addClass("row parent");
+  let para = $("<p></p>").text(moment().hour(9).add(i, 'hours').format("hA"));
+
+  para.addClass("col-md-3");
+  let textArea = $("<textarea class="+time+" 'style= padding left: 100px' />").addClass("col-md-6");
+  let linkElement = $("<button class='saveBtn'><i class='fas fa-unlock'></i></button>").addClass("col-md-3");
+  linkElement.on("click", function()
+  {
+    console.log("button clicked");
+    console.log(textArea.val());
+    localStorage.setItem("textAreaValue"+i,textArea.val());
+  });
+  textArea.html(localStorage.getItem("textAreaValue"+i));
+  $(".container").append(parentElement, para, textArea, linkElement);
+}
+
